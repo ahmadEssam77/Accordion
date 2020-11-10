66402a0a54985c03e5ab131e8b98ce81f16da450
ahmad essam <ahmad77essam@gmail.com>
2020-11-10 19:53:44 +0200

    plus, minus sign, and transition

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-// var element = document.getElementsByClassName("accordion");
-// var i;
-// for (i = 0; i < element.length; i++) {
-//     element[i].addEventListener("click", function() {
-//         this.classList.toggle("active"); // toggle between adding and removing active class
-
-//         var content = this.nextElementSibling;
-//         if (content.style.display === "block") {
-//             content.style.display = "none";
-//         } else {
-//             content.style.display = "block";
-//         }
-//     });
-// }
-
-var acc = document.getElementsByClassName("accordion");
+var element = document.getElementsByClassName("accordion");
 var i;
+for (i = 0; i < element.length; i++) {
+    element[i].addEventListener("click", function() {
+        this.classList.toggle("active"); // toggle between adding and removing active class
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+        var content = this.nextElementSibling;  // next elemnt sibling from button is div
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+
+        if (content.style.maxHeight) {       // transition - animation
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
